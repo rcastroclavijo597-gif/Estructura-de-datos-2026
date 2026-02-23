fcfcc6d8a73143e85f4cab41903f9d3ff4171d37
rcastroclavijo597-gif <rcastroclavijo597@gmail.com>
2026-02-22 22:38:22 -0500

    Create Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double precio;
+        int cantidad;
+        double descuento;
+        double total;
+        double totalConDescuento;
+
+        System.out.print("Ingrese el precio del producto: ");
+        precio = sc.nextDouble();
+
+        System.out.print("Ingrese la cantidad comprada: ");
+        cantidad = sc.nextInt();
+
+        System.out.print("Ingrese el descuento (%): ");
+        descuento = sc.nextDouble();
+
+        total = precio * cantidad;
+
+        totalConDescuento = total - (total * descuento / 100);
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+
+        System.out.println("----- FACTURA -----");
+        System.out.println("Precio unitario: " + precio);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Descuento: " + descuento + "%");
+        System.out.println("Total a pagar: " + totalConDescuento);
+
+        sc.close();
+    }
+}
