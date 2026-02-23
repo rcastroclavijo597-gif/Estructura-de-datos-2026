03b82f77ff6d594c232734693c18a7d7eb7327ca
rcastroclavijo597-gif <rcastroclavijo597@gmail.com>
2026-02-22 22:42:12 -0500

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double temperatura;
+        String nombreSensor;
+        int numeroLectura;
+        boolean enAlarma = false;
+
+        System.out.print("Ingrese el nombre del sensor: ");
+        nombreSensor = sc.nextLine();
+
+        System.out.print("Ingrese la temperatura actual (Celsius): ");
+        temperatura = sc.nextDouble();
+
+        System.out.print("Ingrese el numero de lectura: ");
+        numeroLectura = sc.nextInt();
+
+        if (temperatura > 80) {
+            enAlarma = true;
+        }
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+
+        System.out.println("----- DATOS DEL SENSOR -----");
+        System.out.println("Nombre del sensor: " + nombreSensor);
+        System.out.println("Temperatura: " + temperatura + " C");
+        System.out.println("Numero de lectura: " + numeroLectura);
+        System.out.println("En alarma: " + enAlarma);
+
+        sc.close();
+    }
+}
