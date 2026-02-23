a5b717e1228aca435bea0908f909de97927b5df9
rcastroclavijo597-gif <rcastroclavijo597@gmail.com>
2026-02-22 22:45:05 -0500

    Create ejercicio4.java

---
+++ ejercicio4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String placa;
+        int anio;
+        int cilindraje;
+        double precio;
+        char color;
+        boolean soat;
+
+        System.out.print("Ingrese la placa del vehiculo: ");
+        placa = sc.nextLine();
+
+        System.out.print("Ingrese el año de fabricacion: ");
+        anio = sc.nextInt();
+
+        System.out.print("Ingrese el cilindraje (cc): ");
+        cilindraje = sc.nextInt();
+
+        System.out.print("Ingrese el precio de compra: ");
+        precio = sc.nextDouble();
+
+        System.out.print("Ingrese la inicial del color: ");
+        color = sc.next().charAt(0);
+
+        System.out.print("Tiene SOAT vigente? (true/false): ");
+        soat = sc.nextBoolean();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+
+        System.out.println("----- DATOS DEL VEHICULO -----");
+        System.out.println("Placa: " + placa);
+        System.out.println("Año de fabricacion: " + anio);
+        System.out.println("Cilindraje: " + cilindraje + " cc");
+        System.out.println("Precio de compra: " + precio);
+        System.out.println("Color (inicial): " + color);
+        System.out.println("SOAT vigente: " + soat);
+
+        sc.close();
+    }
+}
