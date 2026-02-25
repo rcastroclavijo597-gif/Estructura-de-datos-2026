7feabae64b5f97364a576dad1f821c3491aef784
rcastroclavijo597-gif <rcastroclavijo597@gmail.com>
2026-02-25 06:53:56 -0500

    ejercicios

--- src/ejercicio9.java
+++ src/ejercicio9.java
@@ -12,7 +12,7 @@ public class ejercicio9 {
         notasCoordinador[1] = 3.0;
 
         System.out.println("notas despues de la correccion:");
-        System.out.println("estudiante 2 (profesor): " + notasProfesor[1]);
+        System.out.println("estudiante 2 (profesor): " + notasProfesor);
         System.out.println("estudiante 2 (coordinador): " + notasCoordinador[1]);
 
         /*
