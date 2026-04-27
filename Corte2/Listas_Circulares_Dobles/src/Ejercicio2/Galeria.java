98d7bd164d600bc3c2c84e72570398c55ac84391
rcastroclavijo597-gif <rcastroclavijo597@gmail.com>
2026-04-27 06:41:53 -0500

    1

---
+++ Corte2/Listas_Circulares_Dobles/src/Ejercicio2/Galeria.java
@@ -0,0 +1,12 @@
+package Corte2.Listas_Circulares_Dobles.src.Ejercicio2;
+
+public class Galeria {
+    Fotos cabeza;
+    fotos cola;
+
+    public Galeria() {
+        this.cabeza = null;
+        this.cola = null;
+    }
+
+}
